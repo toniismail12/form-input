1cce70d64718259941b4b9e9b15e71a703fb2d63
Toni Ismail <92683479+toniismail12@users.noreply.github.com>
2021-11-11 11:45:54 +0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registrasi</title>
+</head>
+<body>
+ <h1>Selamat Datang <?php echo $_POST['nama_depan'] ?> </h1>
+    <h3>Terima kasih telah bergabung di Sanbercode</h3>
+</body>
+</html>
